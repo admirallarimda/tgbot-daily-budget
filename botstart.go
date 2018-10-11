769b3f508f4bdaa8a0464e33212858616db01078
Ilya Lavrinov <ilya.v.lavrinov@gmail.com>
2018-10-11 12:45:36 +0300

    Start via tgbotbase (draft)

--- botstart.go
+++ botstart.go
@@ -1,18 +1,48 @@
 package main
 
 import "log"
-import "github.com/admirallarimda/tgbot-daily-budget/botcfg"
-import "github.com/admirallarimda/tgbot-daily-budget/bot"
+import "gopkg.in/gcfg.v1"
+import "github.com/admirallarimda/tgbotbase"
 
-const cfg_filename = "bot.cfg"
+//import "github.com/admirallarimda/tgbot-daily-budget/bot"
 
-func main() {
-	cfg, err := botcfg.Read(cfg_filename)
+type config struct {
+	tgbotbase.Config
+	Redis tgbotbase.RedisConfig
+}
+
+func readGcfg(filename string) config {
+	log.Printf("Reading configuration from: %s", filename)
+
+	var cfg config
+
+	err := gcfg.ReadFileInto(&cfg, filename)
 	if err != nil {
-		log.Fatalf("Could not read config file %s, exiting with error: %s", cfg_filename, err)
+		log.Printf("Could not correctly parse configuration file: %s; error: %s", filename, err)
+		panic(err)
 	}
 
-	log.Printf("Starting the bot using config: %+v", cfg)
-	bot.Start(cfg)
-	log.Printf("Bot has finished working")
+	log.Printf("Configuration has been successfully read from %s: %+v", filename, cfg)
+	return cfg
+}
+
+func main() {
+	log.Print("Starting daily budget bot")
+
+	cfg := readGcfg("bot.cfg")
+	botCfg := tgbotbase.Config{TGBot: cfg.TGBot, Proxy_SOCKS5: cfg.Proxy_SOCKS5}
+	bot := tgbotbase.NewBot(botCfg)
+
+	/*
+		triggers = addHandler(&startHandler{}, "start", channels, triggers)
+		triggers = addHandler(&transactionHandler{}, "transaction", channels, triggers)
+		triggers = addHandler(&regularTransactionHandler{}, "regular transactions", channels, triggers)
+		triggers = addHandler(&dailyReminder{}, "daily wallet status notification", channels, triggers)
+		triggers = addHandler(&settingsHandler{}, "wallet settings", channels, triggers)
+		triggers = addHandler(&lastTransactionsListHandler{}, "list of last transactions", channels, triggers)
+	*/
+
+	bot.Start()
+
+	log.Print("Daily budget bot has stopped")
 }
